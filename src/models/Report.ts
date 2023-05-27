4475909556953f33631dad3e0fae435bb1fd215d
AlberttoCastro <albertto.programmer@gmail.com>
2023-05-26 21:28:29 -0300

    Include name on report

--- src/models/Report.ts
+++ src/models/Report.ts
@@ -5,13 +5,15 @@ export default class Report {
   hours: number = 0
   visits: number = 0
   studies: number = 0
+  name: string
 
-  constructor (month: Date, literatures, videos, hours, visits, studies) {
+  constructor (month: Date, literatures: number, videos: number, hours: number, visits: number, studies: number, name: string) {
     this.month = month
     this.literatures = literatures
     this.videos = videos
     this.hours = hours
     this.visits = visits
     this.studies = studies
+    this.name = name
   }
 }
