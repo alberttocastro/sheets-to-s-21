f73264470d5c9ee3d748d2085f7d54109a2c7e7a
Alberto Castro Bandeira de Mello <albertto.programmer@gmail.com>
2023-08-08 15:00:22 -0300

    Home page

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,7 @@
+<template>
+  <!-- Create page where there is a beautiful welcome text in the center of the screen -->
+  <div>
+    <h1>Welcome to the Vue.js app</h1>
+    <input type="file" />
+  </div>
+</template>
