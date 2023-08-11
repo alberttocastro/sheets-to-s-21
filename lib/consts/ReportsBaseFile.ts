4e9f01972d1de01ccf4d97bbf4270276f13fc64d
Alberto Castro Bandeira de Mello <albertto.programmer@gmail.com>
2023-08-11 14:24:45 -0300

    create spreadsheet service

---
+++ lib/consts/ReportsBaseFile.ts
@@ -0,0 +1,52 @@
+export let Publishers = [
+  {
+    A: 'Nome',
+    B: 'Grupo',
+    C: 'Servo Ministerial?',
+    D: 'Ancião',
+    E: 'Superintendente?',
+    F: 'Ajudante?',
+    G: 'Pioneiro Regular',
+    H: 'Pioneiro Auxiliar Tempo Indeterminado',
+    I: 'Inativo?',
+    J: 'Homem?',
+    K: 'Participa Emblemas?',
+    L: 'Nascimento',
+    M: 'Idade',
+    N: 'Batismo',
+    O: 'Tempo de Batismo',
+  }
+]
+
+export let Reports = [
+  {
+    A: 'Grupo',
+    B: 'Mês e Ano',
+    C: 'Publicador',
+    D: 'Publicações',
+    E: 'Videos',
+    F: 'Horas',
+    G: 'Revisitas',
+    H: 'Estudos',
+    I: 'Observações',
+    J: 'Pioneiro Regular?',
+    K: 'Pioneiro Auxiliar?',
+    L: 'Aux',
+    M: 'Entrega',
+  }
+]
+
+export let Snapshots = [
+  {
+    A: 'Mês e Ano',
+    B: 'Agrupamento',
+    C: 'Publicações',
+    D: 'Videos',
+    E: 'Horas',
+    F: 'Revisitas',
+    G: 'Estudos',
+    H: 'Observações',
+    I: 'Quantidade',
+    J: 'Total Ativos'
+  }
+]
---
+++ lib/services/CreateSpreadsheetService.ts
@@ -0,0 +1,34 @@
+import * as XLSX from 'xlsx'
+import * as cpexcel from 'xlsx/dist/cpexcel.full.mjs';
+import { Readable } from 'stream';
+import { Publishers, Reports, Snapshots } from '../consts/ReportsBaseFile.ts'
+
+export default class CreateSpreadsheetService {
+
+  constructor () {
+    /* load 'stream' for stream support */
+    XLSX.stream.set_readable(Readable);
+
+    /* load the codepage support library for extended support with older formats  */
+    XLSX.set_cptable(cpexcel);
+  }
+
+  //use the XLSX library and create an excel file
+  public createFile() {
+    const wb = XLSX.utils.book_new();
+
+    const publishers = XLSX.utils.json_to_sheet(Publishers);
+    const reports = XLSX.utils.json_to_sheet(Reports);
+    const snapshots = XLSX.utils.json_to_sheet(Snapshots);
+
+    XLSX.utils.book_append_sheet(wb, publishers, "Publicadores");
+    XLSX.utils.book_append_sheet(wb, reports, "Relatórios");
+    XLSX.utils.book_append_sheet(wb, snapshots, "Resumo");
+
+    return wb
+  }
+
+  public writeFile(path: string, wb: XLSX.WorkBook) {
+    XLSX.writeFile(wb, path)
+  }
+}
