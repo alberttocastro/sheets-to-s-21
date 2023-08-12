a449213ba79d26b4737a6de1647a0379dbceb1b5
Alberto Castro Bandeira de Mello <albertto.programmer@gmail.com>
2023-08-12 09:19:40 -0300

    Adds main layout

---
+++ src/components/layouts/MainLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- Use vuetify to create a template with an application navigator, written the application name and place the content within -->
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>Baruque</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <slot></slot>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "MainLayout",
+});
+</script>
